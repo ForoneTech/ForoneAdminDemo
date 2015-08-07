603a9c35700d0b02ad9fb8aee6530e70cea4ac39
xiaowan <zhangxiaowan@rsct.com>
2015-08-07 14:53:54 +0800

    first time

---
+++ app/Http/Controllers/Tag/TagController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers\Tag;
+
+use App\Http\Requests\CreateTagRequest;
+use App\Http\Requests\UpdateTagRequest;
+use App\Tag;
+use Forone\Admin\Controllers\BaseController;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+
+class TagController extends BaseController
+{
+    const URI = 'tags';
+    const NAME = '标签';
+
+    function __construct()
+    {
+        parent::__construct();
+        view()->share('page_name', self::NAME);
+        view()->share('uri', self::URI);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $results = [
+            'columns' => [
+                ['编号', 'id'],
+                ['标签名', 'name'],
+                ['创建时间', 'created_at'],
+                ['更新时间', 'updated_at'],
+                ['操作', 'buttons', function ($data) {
+                    $buttons = [
+                        ['查看'],
+                        ['编辑'],
+                    ];
+                    return $buttons;
+                }]
+            ]
+        ];
+        $paginate = Tag::paginate();
+        $results['items'] = $paginate;
+        return $this->view(self::URI.'.index',compact('results'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        return $this->view(self::URI.'.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(CreateTagRequest $request)
+    {
+        $data=$request->only('name');
+        Tag::create($data);
+        return redirect()->route('admin.tags.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        $data = Tag::findOrFail($id);
+        if ($data) {
+            return view(self::URI."/show", compact('data'));
+        }else{
+            return $this->redirectWithError('数据未找到');
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        $data = Tag::find($id);
+        if ($data) {
+            return view(self::URI."/edit", compact('data'));
+        } else {
+            return $this->redirectWithError('数据未找到');
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(UpdateTagRequest $request, $id)
+    {
+        $data = $request->only(['name']);
+        Tag::findOrFail($id)->update($data);
+        return redirect()->route('admin.tags.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
